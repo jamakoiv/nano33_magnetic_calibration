01a619922a008f6fd666b30c67a954e8206ca378
Jaakko Koivisto <jaakko.m.koivisto@gmail.com>
2025-02-09 22:07:58 +0200

    Added tests for ellipsoid.py Amend: Forgot to save file.

---
+++ src/tests/test_ellipsoid.py
@@ -0,0 +1,83 @@
+import unittest
+import numpy as np
+
+from PySide6.QtWidgets import QApplication
+
+from ..ellipsoid import makeSphericalMesh, makeEllipsoidXYZ, fitEllipsoidNonRotated
+
+
+class test_ellipsoid(unittest.TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        return super().tearDown()
+
+    def test_makeSphericalMesh(self) -> None:
+        correct_theta = np.array(
+            [
+                [0.0, 0.78539816, 1.57079633, 2.35619449, 3.14159265],
+                [0.0, 0.78539816, 1.57079633, 2.35619449, 3.14159265],
+                [0.0, 0.78539816, 1.57079633, 2.35619449, 3.14159265],
+                [0.0, 0.78539816, 1.57079633, 2.35619449, 3.14159265],
+                [0.0, 0.78539816, 1.57079633, 2.35619449, 3.14159265],
+            ]
+        )
+        correct_phi = np.array(
+            [
+                [0.0, 0.0, 0.0, 0.0, 0.0],
+                [1.57079633, 1.57079633, 1.57079633, 1.57079633, 1.57079633],
+                [3.14159265, 3.14159265, 3.14159265, 3.14159265, 3.14159265],
+                [4.71238898, 4.71238898, 4.71238898, 4.71238898, 4.71238898],
+                [6.28318531, 6.28318531, 6.28318531, 6.28318531, 6.28318531],
+            ]
+        )
+
+        try:
+            theta, phi = makeSphericalMesh(5)
+
+            np.testing.assert_array_almost_equal(theta, correct_theta)
+            np.testing.assert_array_almost_equal(phi, correct_phi)
+
+            self.assertTrue(True)
+        except AssertionError:
+            self.assertTrue(False)
+
+    def test_makeEllipsoidXYZ(self) -> None:
+        correct = np.array(
+            [
+                [10.0, 10.0, 35.0],
+                [20.60660172, 10.0, 27.67766953],
+                [25.0, 10.0, 10.0],
+                [20.60660172, 10.0, -7.67766953],
+                [10.0, 10.0, -15.0],
+                [10.0, 10.0, 35.0],
+                [10.0, 24.14213562, 27.67766953],
+                [10.0, 30.0, 10.0],
+                [10.0, 24.14213562, -7.67766953],
+                [10.0, 10.0, -15.0],
+                [10.0, 10.0, 35.0],
+                [-0.60660172, 10.0, 27.67766953],
+                [-5.0, 10.0, 10.0],
+                [-0.60660172, 10.0, -7.67766953],
+                [10.0, 10.0, -15.0],
+                [10.0, 10.0, 35.0],
+                [10.0, -4.14213562, 27.67766953],
+                [10.0, -10.0, 10.0],
+                [10.0, -4.14213562, -7.67766953],
+                [10.0, 10.0, -15.0],
+                [10.0, 10.0, 35.0],
+                [20.60660172, 10.0, 27.67766953],
+                [25.0, 10.0, 10.0],
+                [20.60660172, 10.0, -7.67766953],
+                [10.0, 10.0, -15.0],
+            ]
+        )
+
+        try:
+            res = makeEllipsoidXYZ(10, 10, 10, 15, 20, 25, 5)
+            np.testing.assert_array_almost_equal(res, correct)
+
+            self.assertTrue(True)
+        except AssertionError:
+            self.assertTrue(False)
