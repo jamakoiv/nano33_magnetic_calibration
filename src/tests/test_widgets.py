c5028a747f907dd42db01738ec5d5cbe189241bc
Jaakko Koivisto <jaakko.m.koivisto@gmail.com>
2025-02-10 15:21:02 +0200

    Added tests for deviceSelectWidget.

---
+++ src/tests/test_widgets.py
@@ -0,0 +1,46 @@
+import unittest
+import numpy as np
+
+from PySide6.QtCore import QAbstractItemModel, Qt
+from PySide6.QtTest import QTest, QSignalSpy
+from PySide6.QtWidgets import QApplication
+
+from ..widgets import DeviceSelectWidget
+
+
+class test_DeviceSelectWidget(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.app = QApplication.instance() or QApplication()
+        return super().setUpClass()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.app.quit()
+        del cls.app
+
+        return super().tearDownClass()
+
+    def setUp(self) -> None:
+        self.widget = DeviceSelectWidget()
+
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        del self.widget
+
+        return super().tearDown()
+
+    def test_data_points_default_value(self) -> None:
+        self.assertEqual(self.widget.data_points.value(), 20)
+
+    def test_data_points_default_step(self) -> None:
+        self.widget.data_points.stepUp()
+        self.assertEqual(self.widget.data_points.value(), 30)
+
+    def test_data_points_range(self) -> None:
+        self.widget.data_points.stepBy(10000)
+        self.assertEqual(self.widget.data_points.value(), 500)
+
+        self.widget.data_points.stepBy(-20000)
+        self.assertEqual(self.widget.data_points.value(), 10)
