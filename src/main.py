e9d2d5d362a0729ee7d4b7661d46ac8a7770f9fe
Jaakko Koivisto <jaakko.m.koivisto@gmail.com>
2025-10-22 12:56:14 +0300

    Refactored main to use functions. Easier testing.

--- src/main.py
+++ src/main.py
@@ -5,22 +5,33 @@ import argparse
 from PySide6.QtWidgets import QApplication
 from gui import MainWindow
 
-parser = argparse.ArgumentParser(description="")
-parser.add_argument(
-    "-v", "--verbose", action="count", default=0, help="Set program output level"
-)
-args = parser.parse_args()
 
-verbosityLevels = {0: logging.WARNING, 1: logging.INFO, 2: logging.DEBUG}
-logging.basicConfig(level=verbosityLevels[args.verbose])
-log = logging.getLogger(__name__)
+def parse_args(args: list[str] = sys.argv[1:]) -> argparse.Namespace:
+    # breakpoint()
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument(
+        "-v", "--verbose", action="count", default=0, help="Set program output level"
+    )
+    return parser.parse_args(args)
 
-# INFO: For now we will use 'info' since matplotlib outputs a lot of stuff at debug-level.
-log.info("Program started with arguments: " + str(args))
 
-if __name__ == "__main__":
+def main() -> None:
+    args = parse_args()
+
+    verbosityLevels = {0: logging.WARNING, 1: logging.INFO, 2: logging.DEBUG}
+    logging.basicConfig(level=verbosityLevels[args.verbose])
+    log = logging.getLogger(__name__)
+
+    # INFO: For now we will use 'info' since matplotlib
+    # outputs a lot of stuff at debug-level.
+    log.info("Program started with arguments: " + str(args))
+
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
 
     sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
