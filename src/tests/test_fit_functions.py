6e9ad541020ea1aa0eb73dc2d7a0af92ed4e46b1
Jaakko Koivisto <jaakko.m.koivisto@gmail.com>
2025-10-23 12:26:17 +0300

    Added tests for refine_rotation_matrix.

--- src/tests/test_fit_functions.py
+++ src/tests/test_fit_functions.py
@@ -56,3 +56,217 @@ class test_fit_functions(unittest.TestCase):
     def test_fit_ellipsoid_rotated(self) -> None: ...
 
     def test_fit_ellipsoid_rotated_alt(self) -> None: ...
+
+    def test_refine_rotation_matrix_identity(self) -> None:
+        # INFO: Check identity-matrix is left unmodified.
+        gain = np.ones(3)
+        rot = np.eye(3)
+
+        res_gain, res_rot = fit_functions.refine_rotation_matrix(gain, rot)
+        np.testing.assert_allclose(res_gain, gain)
+        np.testing.assert_allclose(res_rot, rot)
+
+    def test_refine_rotation_matrix_float_diagonal(self) -> None:
+        gain = np.array([1, 2, 3])
+        rot = np.array(
+            [
+                [0.90, 0.01, 0.02],  # noqa
+                [0.03, 0.80, 0.04],
+                [0.05, 0.06, 0.70],
+            ]
+        )
+
+        res_gain, res_rot = fit_functions.refine_rotation_matrix(gain, rot)
+
+        np.testing.assert_allclose(res_rot, rot)
+        np.testing.assert_allclose(res_gain, gain)
+
+    def test_refine_rotation_matrix_ones_off_diagonal_3x3(self) -> None:
+        gain = np.array([1, 2, 3])
+        rot = np.array(
+            [
+                [0, 0, 1],  # noqa
+                [1, 0, 0],
+                [0, 1, 0],
+            ]
+        )
+
+        correct_gain = np.array([3, 1, 2])
+        correct_rot = np.eye(3)
+
+        res_gain, res_rot = fit_functions.refine_rotation_matrix(gain, rot)
+
+        np.testing.assert_allclose(res_rot, correct_rot)
+        np.testing.assert_allclose(res_gain, correct_gain)
+
+    def test_refine_rotation_matrix_ones_off_diagonal_2x2(self) -> None:
+        gain = np.array([1, 2, 3])
+        rot = np.array(
+            [
+                [1, 0, 0],  # noqa
+                [0, 0, 1],
+                [0, 1, 0],
+            ]
+        )
+
+        correct_gain = np.array([1, 3, 2])
+        correct_rot = np.eye(3)
+
+        res_gain, res_rot = fit_functions.refine_rotation_matrix(gain, rot)
+
+        np.testing.assert_allclose(res_rot, correct_rot)
+        np.testing.assert_allclose(res_gain, correct_gain)
+
+    def test_refine_rotation_matrix_float_off_diagonal_3x3(self) -> None:
+        # INFO: Tests where the 3x3 matrix is reordered, but the 2x2 submatrix is not.
+
+        # NOTE: Case rm == 0.
+        gain = np.array([1, 2, 3])
+        rot = np.array(
+            [
+                [0.03, 0.90, 0.04],  # noqa
+                [0.80, 0.01, 0.02],
+                [0.05, 0.06, 0.70],
+            ]
+        )
+        correct_gain = np.array([2, 1, 3])
+        correct_rot = np.array(
+            [
+                [0.90, 0.03, 0.04],  # noqa
+                [0.01, 0.80, 0.02],
+                [0.06, 0.05, 0.70],
+            ]
+        )
+        res_gain, res_rot = fit_functions.refine_rotation_matrix(gain, rot)
+        np.testing.assert_allclose(res_rot, correct_rot)
+        np.testing.assert_allclose(res_gain, correct_gain)
+
+        # NOTE: Case rm == 1.
+        gain = np.array([1, 2, 3])
+        rot = np.array(
+            [
+                [0.03, 0.80, 0.04],  # noqa
+                [0.90, 0.01, 0.02],
+                [0.05, 0.06, 0.70],
+            ]
+        )
+        correct_gain = np.array([2, 1, 3])
+        correct_rot = np.array(
+            [
+                [0.80, 0.03, 0.04],  # noqa
+                [0.01, 0.90, 0.02],
+                [0.06, 0.05, 0.70],
+            ]
+        )
+        res_gain, res_rot = fit_functions.refine_rotation_matrix(gain, rot)
+        np.testing.assert_allclose(res_rot, correct_rot)
+        np.testing.assert_allclose(res_gain, correct_gain)
+
+        # NOTE: Case rm == 2.
+        gain = np.array([1, 2, 3])
+        rot = np.array(
+            [
+                [0.04, 0.03, 0.80],  # noqa
+                [0.02, 0.70, 0.01],
+                [0.90, 0.05, 0.06],
+            ]
+        )
+        correct_gain = np.array([3, 2, 1])
+        correct_rot = np.array(
+            [
+                [0.80, 0.03, 0.04],  # noqa
+                [0.01, 0.70, 0.02],
+                [0.06, 0.05, 0.90],
+            ]
+        )
+        res_gain, res_rot = fit_functions.refine_rotation_matrix(gain, rot)
+        np.testing.assert_allclose(res_rot, correct_rot)
+        np.testing.assert_allclose(res_gain, correct_gain)
+
+    def test_refine_rotation_matrix_float_off_diagonal_2x2(self) -> None:
+        # INFO: Tests where the 3x3 matrix is not reordered, but the 2x2 submatrix is.
+
+        # NOTE: Case rm == 0.
+        gain = np.array([1, 2, 3])
+        rot = np.array(
+            [
+                [0.90, 0.04, 0.03],  # noqa
+                [0.01, 0.02, 0.80],
+                [0.06, 0.70, 0.05],
+            ]
+        )
+        correct_gain = np.array([1, 3, 2])
+        correct_rot = np.array(
+            [
+                [0.90, 0.04, 0.03],  # noqa
+                [0.01, 0.80, 0.02],
+                [0.06, 0.05, 0.70],
+            ]
+        )
+        res_gain, res_rot = fit_functions.refine_rotation_matrix(gain, rot)
+        np.testing.assert_allclose(res_rot, correct_rot)
+        np.testing.assert_allclose(res_gain, correct_gain)
+
+        # NOTE: Case rm == 1.
+        gain = np.array([1, 2, 3])
+        rot = np.array(
+            [
+                [0.04, 0.03, 0.80],  # noqa
+                [0.02, 0.90, 0.01],
+                [0.70, 0.05, 0.06],
+            ]
+        )
+        correct_gain = np.array([3, 2, 1])
+        correct_rot = np.array(
+            [
+                [0.80, 0.03, 0.04],  # noqa
+                [0.02, 0.90, 0.01],
+                [0.06, 0.05, 0.70],
+            ]
+        )
+        res_gain, res_rot = fit_functions.refine_rotation_matrix(gain, rot)
+        np.testing.assert_allclose(res_rot, correct_rot)
+        np.testing.assert_allclose(res_gain, correct_gain)
+
+        # NOTE: Case rm == 2.
+        gain = np.array([1, 2, 3])
+        rot = np.array(
+            [
+                [0.03, 0.80, 0.04],  # noqa
+                [0.70, 0.01, 0.02],
+                [0.05, 0.06, 0.90],
+            ]
+        )
+        correct_gain = np.array([2, 1, 3])
+        correct_rot = np.array(
+            [
+                [0.80, 0.03, 0.04],  # noqa
+                [0.01, 0.70, 0.02],
+                [0.05, 0.06, 0.90],
+            ]
+        )
+        res_gain, res_rot = fit_functions.refine_rotation_matrix(gain, rot)
+        np.testing.assert_allclose(res_rot, correct_rot)
+        np.testing.assert_allclose(res_gain, correct_gain)
+
+    def test_refine_rotation_matrix_negative_diagonal(self):
+        gain = np.array([1, 2, 3])
+        rot = np.array(
+            [
+                [-0.9, 0.1, 0.1],  # noqa
+                [0.1, -0.8, 0.1],
+                [0.1, 0.1, -0.7],
+            ]
+        )
+        correct_gain = np.array([1, 2, 3])
+        correct_rot = np.array(
+            [
+                [0.9, -0.1, -0.1],  # noqa
+                [-0.1, 0.8, -0.1],
+                [-0.1, -0.1, 0.7],
+            ]
+        )
+
+        res_gain, res_rot = fit_functions.refine_rotation_matrix(gain, rot)
+        np.testing.assert_allclose(res_rot, correct_rot)
+        np.testing.assert_allclose(res_gain, correct_gain)
